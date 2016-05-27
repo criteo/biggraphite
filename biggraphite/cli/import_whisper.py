742fffe526fdedc575607b29c9a9b1f66008b8ad
Brice Arnould <b.arnould@criteo.com>
2016-05-27 17:33:55 +0200

    Add bg-import-whisper and tests.
    This packages the existing import_whisper, fixes bugs and adds tests.

--- biggraphite/cli/import_whisper.py
+++ biggraphite/cli/import_whisper.py
@@ -18,6 +18,7 @@ from __future__ import print_function
 
 import argparse
 import multiprocessing
+from multiprocessing import dummy as multiprocessing_dummy
 import os
 import struct
 import sys
@@ -26,6 +27,9 @@ import progressbar
 import whisper
 
 from biggraphite import accessor as bg_accessor
+
+
+_DEV_NULL = open(os.devnull, "w")
 
 
 _POINT_STRUCT = struct.Struct(whisper.pointFormat)
@@ -39,9 +43,23 @@ class _Worker(object):
             keyspace=opts.keyspace,
             contact_points=opts.contact_points,
             port=opts.port,
-            executor_threads=opts.connections_per_process,
+            concurrency=opts.connections_per_process,
         )
         self._opts = opts
+
+    @staticmethod
+    def _read_metadata(metric_name, path):
+        info = whisper.info(path)
+        retentions = [
+            (a["secondsPerPoint"], a["points"])
+            for a in info["archives"]
+        ]
+        return bg_accessor.MetricMetadata(
+            name=metric_name,
+            carbon_aggregation=info["aggregationMethod"],
+            carbon_retentions=retentions,
+            carbon_xfilesfactor=info["xFilesFactor"],
+        )
 
     @staticmethod
     def _read_points(path):
@@ -51,15 +69,15 @@ class _Worker(object):
         if not info:
             return []
 
-        archives = info['archives']
+        archives = info["archives"]
         with open(path) as f:
             buf = f.read()
         for archive in archives:
-            offset = archive['offset']
-            step = archive['secondsPerPoint']
+            offset = archive["offset"]
+            step = archive["secondsPerPoint"]
             expected_next_timestamp = 0
 
-            for _ in range(archive['points']):
+            for _ in range(archive["points"]):
                 timestamp, val = _POINT_STRUCT.unpack_from(buf, offset)
                 # Detect holes in data. The heuristic is the following:
                 # - If a value is non-zero, it is assumed to be meaningful.
@@ -78,8 +96,10 @@ class _Worker(object):
     def import_whisper(self, path):
         if not self._accessor.is_connected:
             self._accessor.connect()
-        metric_name = path[len(self._opts.root_directory) + 1:-4].replace('/', '.')
+        metric_name = path[len(self._opts.root_directory) + 1:-4].replace("/", ".")
         points = self._read_points(path)
+        meta = self._read_metadata(metric_name, path)
+        self._accessor.create_metric(meta)
         self._accessor.insert_points(metric_name, points)
         return len(points)
 
@@ -90,45 +110,56 @@ def _setup_process(opts):
 
 
 def _import_whisper(*args, **kwargs):
-    assert _WORKER is not None, '_setup_process was never called'
+    assert _WORKER is not None, "_setup_process was never called"
     return _WORKER.import_whisper(*args, **kwargs)
 
 
-def _parse_opts():
-    parser = argparse.ArgumentParser(description='Import whisper files into BigGraphite.')
-    parser.add_argument('root_directory', metavar='WHISPER_DIR',
-                        help='directory in which to find whisper files')
-    parser.add_argument('contact_points', metavar='HOST', nargs='+',
-                        help='hosts used for discovery')
-    parser.add_argument('--keyspace', metavar='NAME',
+def _parse_opts(args):
+    parser = argparse.ArgumentParser(description="Import whisper files into BigGraphite.")
+    parser.add_argument("root_directory", metavar="WHISPER_DIR",
+                        help="directory in which to find whisper files")
+    parser.add_argument("contact_points", metavar="HOST", nargs="+",
+                        help="hosts used for discovery")
+    parser.add_argument("--quiet", action="store_const", default=False, const=True,
+                        help="Show no output unless there are problems.")
+    parser.add_argument("--keyspace", metavar="NAME",
                         help="Cassandra keyspace", default="biggraphite")
-    parser.add_argument('--statements_per_connection', metavar='N', type=int,
-                        help='number of concurrent statements per connection',
+    parser.add_argument("--statements_per_connection", metavar="N", type=int,
+                        help="number of concurrent statements per connection",
                         default=50)
-    parser.add_argument('--port', metavar='PORT', type=int,
-                        help='the native port to connect to', default=9042)
-    parser.add_argument('--connections_per_process', metavar='N', type=int,
-                        help='number of connections per Cassandra host per process', default=4)
-    parser.add_argument('--process', metavar='N', type=int,
-                        help='number of concurrent process', default=multiprocessing.cpu_count())
-    return parser.parse_args()
+    parser.add_argument("--port", metavar="PORT", type=int,
+                        help="the native port to connect to", default=9042)
+    parser.add_argument("--connections_per_process", metavar="N", type=int,
+                        help="number of connections per Cassandra host per process", default=4)
+    parser.add_argument("--process", metavar="N", type=int,
+                        help="number of concurrent process", default=multiprocessing.cpu_count())
+    return parser.parse_args(args)
 
 
-def main():
+def main(args=None):
     """Entry point for the module."""
-    if '__pypy__' not in sys.builtin_module_names:
-        print('Running without PyPy, this is about 20 times slower', file=sys.stderr)
-        sys.stderr.flush()
+    if not args:
+        args = sys.argv
 
-    opts = _parse_opts()
+    opts = _parse_opts(args)
     paths = [os.path.join(root, f)
              for root, _, files in os.walk(opts.root_directory)
-             for f in files if f.endswith('wsp')]
+             for f in files if f.endswith("wsp")]
 
-    pool = multiprocessing.Pool(opts.process, initializer=_setup_process, initargs=(opts,))
+    pool_factory = multiprocessing.Pool
+    if opts.process == 1:
+        pool_factory = multiprocessing_dummy.Pool
+    pool = pool_factory(opts.process, initializer=_setup_process, initargs=(opts,))
+
+    out_fd = sys.stderr
+    if opts.quiet:
+        out_fd = _DEV_NULL
+    if "__pypy__" not in sys.builtin_module_names:
+        print("Running without PyPy, this is about 20 times slower", file=out_fd)
+        out_fd.flush()
 
     total_points = 0
-    with progressbar.ProgressBar(max_value=len(paths), redirect_stderr=True) as pbar:
+    with progressbar.ProgressBar(max_value=len(paths), fd=out_fd, redirect_stderr=True) as pbar:
         for n_path, n_points in enumerate(pool.imap_unordered(_import_whisper, paths)):
             total_points += n_points
             pbar.update(n_path)
@@ -136,7 +167,7 @@ def main():
     pool.close()
     pool.join()
 
-    print('Uploaded', len(paths), 'metrics containing', total_points, 'points')
+    print("Uploaded", len(paths), "metrics containing", total_points, "points", file=out_fd)
 
 if __name__ == "__main__":
     main()
---
+++ tests/test_import_whisper.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Copyright 2016 Criteo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+
+from os import path as os_path
+import tempfile
+import shutil
+import time
+import unittest
+
+import mock
+import whisper
+
+from biggraphite.cli import import_whisper
+from biggraphite import test_utils as bg_test_utils
+
+
+bg_test_utils.prepare_graphite_imports()
+
+
+class TestMain(bg_test_utils.TestCaseWithFakeAccessor):
+
+    def setUp(self):
+        super(TestMain, self).setUp()
+        self.tempdir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.tempdir)
+
+    def test_single_metric(self):
+        xfilesfactor = 0.5
+        aggregation_method = "last"
+        retentions = [(1, 60)]
+        now = int(time.time())
+        time_from, time_to = now - 10, now
+        points = [(t, now-t) for t in xrange(time_from, time_to)]
+        metric = "test_metric"
+        metric_path = os_path.join(self.tempdir, metric + ".wsp")
+        whisper.create(metric_path, retentions, xfilesfactor, aggregation_method)
+        whisper.update_many(metric_path, points)
+
+        self._call_main()
+
+        meta = self.accessor.get_metric(metric)
+        self.assertTrue(meta)
+        self.assertEqual(meta.name, metric)
+        self.assertEqual(meta.carbon_aggregation, aggregation_method)
+        self.assertEqual(meta.carbon_xfilesfactor, xfilesfactor)
+        self.assertEqual(meta.carbon_retentions, retentions)
+
+        points_again = self.accessor.fetch_points(metric, time_from, time_to, step=1)
+        self.assertEqual(points, points_again)
+
+    def _call_main(self):
+        with mock.patch('biggraphite.accessor.Accessor', return_value=self.accessor):
+            import_whisper.main([
+                "--quiet",
+                "--keyspace", "keyspace",
+                "--port", "42",
+                "--process", "1",
+                self.tempdir,
+                "testhost1", "testhost2",
+            ])
+
+
+if __name__ == "__main__":
+    unittest.main()
