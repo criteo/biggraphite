776cbf5e332d1802b47abaed0c2520137db68203
Nathaniel Braun <n.braun@criteo.com>
2016-07-12 19:06:42 +0200

    Implement aggregation mechanism

---
+++ biggraphite/drivers/aggregate.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# Copyright 2016 Criteo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Metric-level & stage-level aggregate interface."""
+
+from __future__ import print_function
+
+
+class StageAggregate(object):
+    """Perform aggregation on the data of a given retention stage."""
+
+    __slots__ = (
+        "precision",
+        "_aggregator",
+        "_epoch",
+        "count",
+        "value"
+    )
+
+    def __init__(self, precision, aggregator):
+        """Initialize a new StageAggregator.
+
+        Args:
+          precision: precision of the stage, in seconds.
+          aggregator: aggregator object to compute aggregates.
+        """
+        self.precision = precision
+        self._aggregator = aggregator
+        self._epoch = None
+        self.count = 0
+        self.value = None
+
+    def compute(self, points):
+        """Compute aggregated value but do not store it.
+
+        The points have to be sorted by increasing timestamps.
+
+        Args:
+          points: new points to be added into the current aggregate.
+
+        Returns:
+          Iterable of (timestamp, count, value).
+        """
+        if self._epoch is None and not points:
+            return []
+
+        res = [(self._epoch, self.count, self.value)]
+        for (timestamp, value) in points:
+            epoch = timestamp // self.precision
+            last_res_point = res[-1]
+            res_epoch = last_res_point[0]
+            if res_epoch is None:
+                # no prior state => take first point
+                res[0] = (epoch, 1, value)
+            elif res_epoch == epoch:
+                # point is in current epoch => aggregate
+                old_value = last_res_point[2]
+                old_count = last_res_point[1]
+                aggregated_value = self._aggregator.merge(old_value, old_count, value)
+                res[-1] = (last_res_point[0], last_res_point[1] + 1, aggregated_value)
+            elif res_epoch < epoch:
+                # point is in new epoch => add new epoch
+                res.append((epoch, 1, value))
+        return [(r[0] * self.precision, r[1], r[2]) for r in res]
+
+    def update(self, points):
+        """Compute aggregated value and store it.
+
+        The points have to be sorted by increasing timestamps.
+
+        Args:
+          points: new points to be added into the current aggregate.
+
+        Returns:
+          Iterable of (timestamp, count, value).
+        """
+        res = self.compute(points)
+        if res:
+            # update current state to last point in res
+            last_res_point = res[-1]
+            self._epoch = last_res_point[0] // self.precision
+            self.count = last_res_point[1]
+            self.value = last_res_point[2]
+        return res
+
+
+class MetricAggregates(object):
+    """Perform aggregation on the data of a given metric."""
+
+    __slots__ = (
+        "_stage",
+    )
+
+    def __init__(self, metric_metadata):
+        """Initialize a new MetricAggregator.
+
+        Args:
+          metric_metadata: MetricMetadata object.
+        """
+        self._stage = [StageAggregate(r.precision, metric_metadata.aggregator)
+                       for r in metric_metadata.retention]
+
+    def compute(self, points):
+        """Compute aggregated values but do not store them.
+
+        The points have to be sorted by increasing timestamps.
+
+        Args:
+          points: new points to be added into the current aggregates.
+
+        Returns:
+          Iterable of iterables of (timestamp, value).
+        """
+        # p[0]: timestamp, rounded to a multiple of the stage precision
+        # p[2]: aggregated value
+        return [[(p[0], p[2]) for p in s.compute(points)]
+                for s in self._stage]
+
+    def update(self, points):
+        """Compute aggregated values and store them.
+
+        The points have to be sorted by increasing timestamps.
+
+        Args:
+          points: new points to be added into the current aggregates.
+
+        Returns:
+          Iterable of iterables of (timestamp, value).
+        """
+        # p[0]: timestamp, rounded to a multiple of the stage precision
+        # p[2]: aggregated value
+        return [[(p[0], p[2]) for p in s.update(points)]
+                for s in self._stage]
---
+++ tests/test_aggregate.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# Copyright 2016 Criteo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import unittest
+
+
+from biggraphite import accessor as bg_accessor
+from biggraphite.drivers import aggregate as bg_aggregate
+
+
+class TestStageAggregates(unittest.TestCase):
+    PRECISION = 60
+
+    def setUp(self):
+        self.sa = bg_aggregate.StageAggregate(self.PRECISION, bg_accessor.Aggregator.average)
+
+    def _test_compute(self, points, expected):
+        """Call compute & check that state is unchanged."""
+        self.assertEqual(expected, self.sa.compute(points))
+
+        self.assertIsNone(self.sa._epoch)
+        self.assertEqual(0, self.sa.count)
+        self.assertIsNone(self.sa.value)
+
+    def test_compute(self):
+        """Test aggregation operations."""
+        points = []
+        expected = []
+        self._test_compute(points, expected)
+
+        points = [(0, 2)]
+        expected = [(0, 1, 2)]
+        self._test_compute(points, expected)
+
+        points = [(0, 2), (1, 4)]
+        expected = [(0, 2, 3)]
+        self._test_compute(points, expected)
+
+        points = [(0, 2), (1, 8), (self.PRECISION, -1)]
+        expected = [(0, 2, 5), (self.PRECISION, 1, -1)]
+        self._test_compute(points, expected)
+
+        points = [(0, 2), (1, 18), (self.PRECISION, -1), (self.PRECISION * 3, 5)]
+        expected = [(0, 2, 10), (self.PRECISION, 1, -1), (3 * self.PRECISION, 1, 5)]
+        self._test_compute(points, expected)
+
+    def test_update(self):
+        """Test aggregation + update operations."""
+        points = [(0, 2)]
+        expected = [(0, 1, 2)]
+        self.assertEqual(expected, self.sa.update(points))
+        self.assertEqual(self.sa._epoch, 0)
+        self.assertEqual(self.sa.count, 1)
+        self.assertEqual(self.sa.value, 2)
+
+        points = []
+        expected = [(0, 1, 2)]
+        self.assertEqual(expected, self.sa.update(points))
+        self.assertEqual(self.sa._epoch, 0)
+        self.assertEqual(self.sa.count, 1)
+        self.assertEqual(self.sa.value, 2)
+
+        points = [(1, 42)]
+        expected = [(0, 2, 22)]
+        self.assertEqual(expected, self.sa.update(points))
+        self.assertEqual(self.sa._epoch, 0)
+        self.assertEqual(self.sa.count, 2)
+        self.assertEqual(self.sa.value, 22)
+
+        points = [(self.PRECISION, -1), (self.PRECISION * 5, -5)]
+        expected = [(0, 2, 22), (self.PRECISION, 1, -1), (5 * self.PRECISION, 1, -5)]
+        self.assertEqual(expected, self.sa.update(points))
+        self.assertEqual(self.sa._epoch, 5)
+        self.assertEqual(self.sa.count, 1)
+        self.assertEqual(self.sa.value, -5)
+
+
+class TestMetricAggregate(unittest.TestCase):
+    PRECISION = 10
+
+    def setUp(self):
+        aggregator = bg_accessor.Aggregator.average
+        precisions = tuple([self.PRECISION ** i for i in [1, 2, 3]])
+        retention_string = "5*%ds:10*%ds:15*%ds" % (precisions)
+        retention = bg_accessor.Retention.from_string(retention_string)
+        metric_metadata = bg_accessor.MetricMetadata(aggregator=aggregator, retention=retention)
+        self.ma = bg_aggregate.MetricAggregates(metric_metadata)
+
+    def test_compute(self):
+        """Test aggregation operations."""
+        points = []
+        expected = [[], [], []]
+        result = self.ma.compute(points)
+        self.assertEqual(result, expected)
+
+        points = [(0, 1)]
+        expected = [[(0, 1)], [(0, 1)], [(0, 1)]]
+        result = self.ma.compute(points)
+        self.assertEqual(result, expected)
+
+        points = [
+            (0, 30),
+            (self.PRECISION, 60),
+            (self.PRECISION ** 2 - 1, 120),
+            (self.PRECISION ** 2, 240),
+            (self.PRECISION ** 3 - 1, 480),
+            (self.PRECISION ** 3, 600)
+        ]
+        expected_stage_0 = [
+            (0, 30),
+            (self.PRECISION, 60),
+            (self.PRECISION * (self.PRECISION - 1), 120),
+            (self.PRECISION ** 2, 240),
+            (self.PRECISION * (self.PRECISION ** 2 - 1), 480),
+            (self.PRECISION ** 3, 600)
+        ]
+        expected_stage_1 = [
+            (0, 70),
+            (self.PRECISION ** 2, 240),
+            (self.PRECISION ** 2 * (self.PRECISION - 1), 480),
+            (self.PRECISION ** 3, 600),
+        ]
+        expected_stage_2 = [
+            (0, 186),
+            (self.PRECISION ** 3, 600)
+        ]
+        expected = [
+            expected_stage_0,
+            expected_stage_1,
+            expected_stage_2
+        ]
+        result = self.ma.compute(points)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
