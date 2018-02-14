90b3184e27c1638ff1da24cd8807b908bb54c477
Corentin Chary <c.chary@criteo.com>
2018-02-14 16:31:44 +0100

    bg-replay-traffic: add some more options

--- biggraphite/cli/replay_traffic.py
+++ biggraphite/cli/replay_traffic.py
@@ -25,6 +25,7 @@ import logging
 import sys
 import time
 import progressbar
+import re
 
 import multiprocessing
 from multiprocessing import dummy as multiprocessing_dummy
@@ -40,6 +41,10 @@ def handle_packet(timestamp, packet):
     """Parse one packet and return the associate HTTP request if any."""
     # Unpack the Ethernet frame (mac src/dst, ethertype)
     eth = dpkt.ethernet.Ethernet(packet)
+    try:
+        eth = dpkt.ethernet.Ethernet(packet)
+    except (dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
+        return None
 
     # Make sure the Ethernet data contains an IP packet
     if not isinstance(eth.data, dpkt.ip.IP):
@@ -75,8 +80,7 @@ def get_requests(filename):
     for timestamp, packet in pcap:
         res = handle_packet(timestamp, packet)
         if res is not None:
-            requests.append(res)
-    return requests
+            yield res
 
 
 class _Worker(object):
@@ -84,11 +88,12 @@ class _Worker(object):
 
     def __init__(self, opts):
         self._opts = opts
+        self._exclude = self._opts.exclude
         self._last_request = 0
         self._last_played_timestamp = 0
 
     def _sleep(self, timestamp):
-        if self._last_played_timestamp:
+        if self._last_played_timestamp and self._opts.time_factor:
             delta = (timestamp - self._last_played_timestamp).total_seconds()
             delta -= (time.time() - self._last_request)
             delta /= self._opts.time_factor
@@ -107,7 +112,12 @@ class _Worker(object):
             data = orig_request.body
         else:
             data = None
-        logging.debug(url, data)
+        if self._exclude:
+            if self._exclude.match(url):
+                return
+            if self._exclude.match(str(data)):
+                return
+        logging.debug("%s: %s", url, data)
         request = clusters_diff.Request(url, auth_key, timeout_s, data)
         request.execute()
 
@@ -122,53 +132,85 @@ def _do_request(*args, **kwargs):
     try:
         return _WORKER.do_request(*args, **kwargs)
     except Exception as e:
-        logging.exception(e)
+        logging.exception("%s" % (args))
         return 0
 
 
 def _parse_opts(args):
     parser = argparse.ArgumentParser(
-        description="Import whisper files into BigGraphite.")
+        description="Replay HTTP Traffic",
+        epilog=(
+            "Capture traffic with:"
+            "sudo tcpdump -i any dst port 80 -s 0 -w graphite.pcap"
+        )
+    )
     parser.add_argument("pcap_files", metavar="PCAP_FILE", nargs="*",
                         help="pcap files to read packets from.")
     parser.add_argument("--process", metavar="N", type=int,
                         help="number of concurrent process",
                         default=multiprocessing.cpu_count())
     parser.add_argument("--time_factor", type=int,
-                        help="Speed up time by this factor.",
+                        help="Speed up time by this factor. 0 to disable.",
                         default=1)
     parser.add_argument("--host", metavar="host", type=str, required=True,
                         help="Host to execute queries on.")
+    parser.add_argument("--exclude", metavar="exclude", type=str,
+                        required=False, default="",
+                        help="Exclude queries matching.")
     parser.add_argument("--timeout", type=int,
                         help="Request timeout",
                         default=60)
+    parser.add_argument("--verbose",
+                        action='store_true', default=False)
+    parser.add_argument("--progress",
+                        action='store_true', default=False)
 
     opts = parser.parse_args(args)
+    if opts.verbose:
+        logging.basicConfig(level=logging.DEBUG)
     return opts
+
+
+def read_requests(opts):
+    if opts.progress:
+        # If we want progress, we read everything.
+        requests = []
+        for filename in opts.pcap_files:
+            requests.extend(get_requests(filename))
+        requests = sorted(requests, key=lambda k: k[0])
+    else:
+        # Else we can be more efficient.
+        for filename in opts.pcap_files:
+            for request in get_requests(filename):
+                yield request
 
 
 def main():
     """Main function."""
     opts = _parse_opts(sys.argv[1:])
-    requests = []
-    for filename in opts.pcap_files:
-        requests.extend(get_requests(filename))
-    requests = sorted(requests, key=lambda k: k[0])
+    if opts.exclude:
+        opts.exclude = re.compile(opts.exclude)
 
+    requests = read_requests(opts)
     pool_factory = multiprocessing.Pool
     if opts.process == 1:
         pool_factory = multiprocessing_dummy.Pool
     pool = pool_factory(
         opts.process, initializer=_setup_process, initargs=(opts,))
 
-    with progressbar.ProgressBar(max_value=len(requests)) as pbar:
-        for i, _ in enumerate(pool.imap_unordered(_do_request, requests)):
+    if opts.progress:
+        pbar = progressbar.ProgressBar(max_value=len(requests))
+    else:
+        pbar = None
+
+    for i, _ in enumerate(pool.imap_unordered(_do_request, requests)):
+        if pbar:
             pbar.update(i)
 
     pool.close()
     pool.join()
 
-    print("Did ", len(requests), " queries")
+    print("Did %s queries" % i)
 
 
 if __name__ == "__main__":
