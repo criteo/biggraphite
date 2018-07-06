ace0b71fd36a2e4a3dbeee48fe5cd114cbf17450
Alban Dericbourg <a.dericbourg@criteo.com>
2018-07-06 14:42:18 +0200

    Add scripts to help running test services
    Includes:
    * Cassandra
    * Carbon
    * Graphite Web
    
    Signed-off-by: Alban Dericbourg <a.dericbourg@criteo.com>

---
+++ scripts/run_carbon.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+cd $(git rev-parse --show-toplevel)
+source scripts/env.sh
+
+cd ${BG_VENV}/conf
+
+if [ ! -f carbon.conf ]; then
+echo "No carbon configuration, creating default one"
+tee -a carbon.conf << END
+[cache]
+# Cassandra configuration
+BG_CASSANDRA_KEYSPACE = biggraphite
+BG_CASSANDRA_CONTACT_POINTS = 127.0.0.1
+BG_DRIVER = cassandra
+BG_CACHE = memory
+DATABASE = biggraphite
+STORAGE_DIR = /tmp
+
+## Elasticsearch configuration
+# BG_DRIVER = elasticsearch
+# BG_CACHE = memory
+# DATABASE = biggraphite
+# STORAGE_DIR = /tmp
+# BG_ELASTICSEARCH_HOSTS = elasticsearch.fqdn
+# BG_ELASTICSEARCH_PORT = 8080
+# BG_ELASTICSEARCH_USERNAME = foo
+# BG_ELASTICSEARCH_PASSWORD = bar
+END
+fi
+
+touch storage-schemas.conf
+bg-carbon-cache --debug --nodaemon --conf=carbon.conf start
