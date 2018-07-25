253dbfcf2a4d245c615cd37309b0d950f26e18a0
Alban Dericbourg <a.dericbourg@criteo.com>
2018-07-25 09:45:03 +0200

    Install dependencies only when they are not present
    Signed-off-by: Alban Dericbourg <a.dericbourg@criteo.com>

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -12,41 +12,48 @@ ES_HOME=$(pwd)"/.deps/elasticsearch-${ES_VERSION}/"
 
 mkdir -p .deps
 
-if [ -n "${CASSANDRA_VERSION}" ]; then
-    cd .deps
-    FILENAME="apache-cassandra-${CASSANDRA_VERSION}-bin.tar.gz"
-    if [ ! -f "${FILENAME}" ]; then
-        wget "http://www.us.apache.org/dist/cassandra/${CASSANDRA_VERSION}/${FILENAME}"
-        tar -xzf "${FILENAME}"
-    fi
-    cd -
-fi
-
-
-if [ -n "${CASSANDRA_STRATIO_LUCENE_VERSION}" ]; then
-    if [ -n "${CASSANDRA_STRATIO_LUCENE_BUILD}" ]; then
+if [ ! -d "${CASSANDRA_HOME}" ]; then
+    echo "Installing Cassandra"
+    if [ -n "${CASSANDRA_VERSION}" ]; then
         cd .deps
-        git clone http://github.com/Stratio/cassandra-lucene-index
-        cd cassandra-lucene-index
-        git checkout ${CASSANDRA_STRATIO_LUCENE_VERSION}
-        git checkout -b ${CASSANDRA_STRATIO_LUCENE_VERSION}
-
-        mvn clean package -DskipTests
-        cp -v plugin/target/cassandra-lucene-index-plugin-*.jar ${CASSANDRA_HOME}/lib/
-    else
-        # Download pre-built binaries
-        wget --content-disposition https://search.maven.org/remotecontent?filepath=com/stratio/cassandra/cassandra-lucene-index-plugin/${CASSANDRA_STRATIO_LUCENE_VERSION}/cassandra-lucene-index-plugin-${CASSANDRA_STRATIO_LUCENE_VERSION}.jar
-        cp -v cassandra-lucene-index-plugin-*.jar ${CASSANDRA_HOME}/lib/
+        FILENAME="apache-cassandra-${CASSANDRA_VERSION}-bin.tar.gz"
+        if [ ! -f "${FILENAME}" ]; then
+            wget "http://www.us.apache.org/dist/cassandra/${CASSANDRA_VERSION}/${FILENAME}"
+            tar -xzf "${FILENAME}"
+        fi
+        cd -
     fi
 fi
 
+if ! ls ${CASSANDRA_HOME}/lib/cassandra-lucene-index-plugin-*.jar 1> /dev/null 2>&1; then
+    echo "Installing Cassandra Statio Lucene plugin"
+    if [ -n "${CASSANDRA_STRATIO_LUCENE_VERSION}" ]; then
+        if [ -n "${CASSANDRA_STRATIO_LUCENE_BUILD}" ]; then
+            cd .deps
+            git clone http://github.com/Stratio/cassandra-lucene-index
+            cd cassandra-lucene-index
+            git checkout ${CASSANDRA_STRATIO_LUCENE_VERSION}
+            git checkout -b ${CASSANDRA_STRATIO_LUCENE_VERSION}
 
-if [ -n "${ES_VERSION}" ]; then
-    cd .deps
-    FILENAME="elasticsearch-${ES_VERSION}.tar.gz"
-    if [ ! -f "${FILENAME}" ]; then
-        wget "https://artifacts.elastic.co/downloads/elasticsearch/${FILENAME}"
-        tar -xzf "${FILENAME}"
+            mvn clean package -DskipTests
+            cp -v plugin/target/cassandra-lucene-index-plugin-*.jar ${CASSANDRA_HOME}/lib/
+        else
+            # Download pre-built binaries
+            wget --content-disposition https://search.maven.org/remotecontent?filepath=com/stratio/cassandra/cassandra-lucene-index-plugin/${CASSANDRA_STRATIO_LUCENE_VERSION}/cassandra-lucene-index-plugin-${CASSANDRA_STRATIO_LUCENE_VERSION}.jar
+            cp -v cassandra-lucene-index-plugin-*.jar ${CASSANDRA_HOME}/lib/
+        fi
     fi
-    cd -
+fi
+
+if [ ! -d "${ES_HOME}" ]; then
+    echo "Installing Elasticsearch"
+    if [ -n "${ES_VERSION}" ]; then
+        cd .deps
+        FILENAME="elasticsearch-${ES_VERSION}.tar.gz"
+        if [ ! -f "${FILENAME}" ]; then
+            wget "https://artifacts.elastic.co/downloads/elasticsearch/${FILENAME}"
+            tar -xzf "${FILENAME}"
+        fi
+        cd -
+    fi
 fi
