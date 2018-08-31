03af6c4b537680cce63c7961879b79066dd75a4f
Geoffrey Beausire <g.beausire@criteo.com>
2018-08-31 13:03:52 +0200

    Optional tracing

--- biggraphite/drivers/elasticsearch.py
+++ biggraphite/drivers/elasticsearch.py
@@ -36,7 +36,11 @@ from biggraphite import metric as bg_metric
 from biggraphite.drivers import _utils
 from biggraphite.drivers import ttls
 
-from opencensus.trace import config_integration
+try:
+    from opencensus.trace import config_integration
+except ImportError:
+    config_integration = None
+
 
 UPDATED_ON = prometheus_client.Summary(
     "bg_elasticsearch_updated_on_latency_seconds", "create latency in seconds"
@@ -305,7 +309,8 @@ class _ElasticSearchAccessor(bg_accessor.Accessor):
         self.__schema_path = schema_path
         self.client = None
         self.schema = None
-        config_integration.trace_integrations(['httplib'])
+        if config_integration:
+            config_integration.trace_integrations(['httplib'])
         log.debug(
             "Created Elasticsearch accessor with index prefix: '%s' and index suffix: '%s'"
             % (self._index_prefix, self._index_suffix)
