e932dc0c817cb979db546e170b06a329da26d9c4
Patrick MARIE <mycroft@users.noreply.github.com>
2021-03-26 16:16:52 +0100

    Multiple cleaning enhancements & fixes. (#560)
    * cleaning: Optionally set number of token to ignore on error.
    
    * README: Adding some info for prometheus metrics testing.
    
    * Cleaning: Improve metrics reporting.
    
    * Cleaning: Catch empty directory cleaning exceptions.

--- biggraphite/drivers/cassandra.py
+++ biggraphite/drivers/cassandra.py
@@ -231,6 +231,12 @@ CLEAN_STOP_TS_METRIC = prometheus_client.Gauge(
     "bg_util_clean_ts_stop",
     "clean last end timestamp"
 )
+CLEAN_STEP_TS = prometheus_client.Gauge(
+    "bg_util_clean_step_duration",
+    "Duration of any step of the cleaning",
+    ['step']
+)
+
 CLEAN_CURRENT_STEP = prometheus_client.Gauge(
     "bg_util_clean_current_step",
     "clean current step (0: none, 1: expired directories, 2: expired metrics)"
@@ -238,6 +244,15 @@ CLEAN_CURRENT_STEP = prometheus_client.Gauge(
 CLEAN_CURRENT_OFFSET = prometheus_client.Gauge(
     "bg_util_clean_current_offset",
     "clean current offset"
+)
+CLEAN_SKIPPED_OFFSET = prometheus_client.Counter(
+    "bg_util_clean_skipped_offset",
+    "skipped offset while cleaning"
+)
+
+ERROR_BIND_METRIC = prometheus_client.Counter(
+    "bg_error_bind_metric",
+    "Number of incomplete or invalid record read from backend"
 )
 
 log = logging.getLogger(__name__)
@@ -1799,6 +1814,11 @@ class _CassandraAccessor(bg_accessor.Accessor):
 
         # Return None if any of the important column is missing.
         if not uid or not config or not updated_on_raw:
+            ERROR_BIND_METRIC.inc()
+            log.warning(
+                "Failed to bind metric; Missing column data: name:%s uid:%s config:%s updated_on:%s"
+                % (metric_name, uid, config, updated_on_raw)
+            )
             return None
 
         updated_on = datetime.datetime.fromtimestamp(updated_on_raw / 1000)
@@ -2352,6 +2372,7 @@ class _CassandraAccessor(bg_accessor.Accessor):
         shard=0,
         nshards=1,
         callback_on_progress=None,
+        num_token_ignore_on_error=DEFAULT_MAX_BATCH_UTIL,
     ):
         """Remove directory that does not contains any metrics."""
         start_token, stop_token = self._get_search_range(
@@ -2400,22 +2421,45 @@ class _CassandraAccessor(bg_accessor.Accessor):
                     dir_name
                 )
 
+        # Reset the error count.
+        ignored_errors = 0
+
         log.info("Starting cleanup of empty dir")
         token = start_token
         while token < stop_token:
             CLEAN_CURRENT_OFFSET.set(token)
-            result = self._execute_metadata(
-                CLEAN_EMPTY_DIR,
-                _CassandraExecutionRequest(
-                    CLEAN,
-                    dir_query,
-                    int(token)
-                ),
-                timeout=DEFAULT_TIMEOUT_QUERY_UTIL
-            )
+            try:
+                result = self._execute_metadata(
+                    CLEAN_EMPTY_DIR,
+                    _CassandraExecutionRequest(
+                        CLEAN,
+                        dir_query,
+                        int(token)
+                    ),
+                    timeout=DEFAULT_TIMEOUT_QUERY_UTIL
+                )
 
-            if len(result.current_rows) == 0:
-                break
+                # Reset the error count.
+                ignored_errors = 0
+
+                # End was reached
+                if len(result.current_rows) == 0:
+                    break
+            except Exception as e:
+                log.exception("Got exception: %s at token %s" % (e, token))
+                # Put sleep a little bit to not stress Cassandra too mutch.
+                time.sleep(1)
+                ignored_errors += 1
+
+                # After a few retries on the same query, lets move on.
+                if ignored_errors % 3 == 0:
+                    token += num_token_ignore_on_error
+                    CLEAN_SKIPPED_OFFSET.inc(num_token_ignore_on_error)
+
+                # If we failed too much, let's just stop the process.
+                if ignored_errors > BATCH_MAX_IGNORED_ERRORS:
+                    break
+                continue
 
             # Update token range for the next iteration
             token = result[-1][1]
@@ -2446,6 +2490,7 @@ class _CassandraAccessor(bg_accessor.Accessor):
         callback_on_progress=None,
         disable_clean_directories=False,
         disable_clean_metrics=False,
+        num_token_ignore_on_error=DEFAULT_MAX_BATCH_UTIL,
     ):
         """See bg_accessor.Accessor.
 
@@ -2456,39 +2501,53 @@ class _CassandraAccessor(bg_accessor.Accessor):
 
         CLEAN_START_TS_METRIC.set(time.time())
         CLEAN_STOP_TS_METRIC.set(0)
-        CLEAN_CURRENT_STEP.set(1)
+        CLEAN_CURRENT_STEP.set(0)
 
         first_exception = None
 
         # First, clean metrics...
         if not disable_clean_metrics:
+            CLEAN_CURRENT_STEP.set(1)
+            ts_start = time.time()
             try:
                 self._clean_expired_metrics(
-                    max_age, start_key, end_key, shard, nshards, callback_on_progress
+                    max_age,
+                    start_key,
+                    end_key,
+                    shard,
+                    nshards,
+                    callback_on_progress,
+                    num_token_ignore_on_error,
                 )
             except Exception as e:
                 first_exception = e
                 log.exception("Failed to clean metrics.")
+            CLEAN_CURRENT_STEP.set(2)
+            CLEAN_STEP_TS.labels('expired_metrics').set(time.time() - ts_start)
         else:
             log.info("Cleaning metrics was disabled")
 
-        CLEAN_CURRENT_STEP.set(2)
-
         # Then, clean directories...
         if not disable_clean_directories:
+            CLEAN_CURRENT_STEP.set(3)
+            ts_start = time.time()
             try:
                 self._clean_empty_dir(
-                    start_key, end_key, shard, nshards, callback_on_progress
+                    start_key,
+                    end_key,
+                    shard,
+                    nshards,
+                    callback_on_progress,
+                    num_token_ignore_on_error,
                 )
             except Exception as e:
                 first_exception = e
                 log.exception("Failed to clean directories.")
+            CLEAN_CURRENT_STEP.set(4)
+            CLEAN_STEP_TS.labels('empty_directories').set(time.time() - ts_start)
         else:
             log.info("Cleaning directories was disabled")
 
-        CLEAN_CURRENT_STEP.set(0)
-        CLEAN_CURRENT_OFFSET.set(0)
-        CLEAN_START_TS_METRIC.set(0)
         CLEAN_STOP_TS_METRIC.set(time.time())
 
         if first_exception is not None:
@@ -2522,6 +2581,7 @@ class _CassandraAccessor(bg_accessor.Accessor):
         shard=1,
         nshards=0,
         callback_on_progress=None,
+        num_token_ignore_on_error=DEFAULT_MAX_BATCH_UTIL,
     ):
         """Delete metrics that has an expired ttl."""
         if not max_age:
@@ -2588,8 +2648,14 @@ class _CassandraAccessor(bg_accessor.Accessor):
                 log.exception("Got exception: %s at token %s" % (e, token))
                 # Put sleep a little bit to not stress Cassandra too mutch.
                 time.sleep(1)
-                token += DEFAULT_MAX_BATCH_UTIL
                 ignored_errors += 1
+
+                # After a few retries on the same query, lets move on.
+                if ignored_errors % 3 == 0:
+                    token += num_token_ignore_on_error
+                    CLEAN_SKIPPED_OFFSET.inc(num_token_ignore_on_error)
+
+                # If we failed too much, let's just stop the process.
                 if ignored_errors > BATCH_MAX_IGNORED_ERRORS:
                     break
                 continue
