f2a09829c609ed8ed68c99761ac9f63376455dc2
Patrick MARIE <pm@mkz.me>
2021-04-12 17:57:50 +0200

    Bump to 0.14.24.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ packages = setuptools.find_packages()
 
 setuptools.setup(
     name="biggraphite",
-    version="0.14.23",
+    version="0.14.24",
     maintainer="Criteo Graphite Team",
     maintainer_email="github@criteo.com",
     description="Simple Scalable Time Series Database.",
